d777e365db1723b9863ec40be7a3abd62bf1a3d7
Ram <Ram@Ram-PC>
2019-12-16 23:22:27 +0530

    update the reverse string

--- LogicalProgramAll/src/com/logic/strings/ReverseString.java
+++ LogicalProgramAll/src/com/logic/strings/ReverseString.java
@@ -15,29 +15,26 @@ public class ReverseString {
 	       String strresver= reverseString(inString);
 	       System.out.println(strresver);
 	        
-		  /*
-	        //single Palindrom using stringbuffer logic
-	        if(isPalindromStringBuffer(inString)){
-	            System.out.println(inString +" is a Palindrom String");
-	        }
-	        else{
-	            System.out.println(inString +" is not a Palindrom String");
-	        }
-	        */
+		
 	        
 	        
 	}//end of the ReverseString 
 
-	//using stringbuffer 
+	/*//using stringbuffer 
 	public static boolean isPalindromStringBuffer(String inputStr){
 	        StringBuilder sb  = new StringBuilder(inputStr);
 	        String reverseStr = sb.reverse().toString();
 	        return (inputStr.equalsIgnoreCase(reverseStr));              
-	}
+	}*/
 
 	
 	//using stringbuffer 
 	public static String reverseString(String inputStr){
+		 if(inputStr == null || inputStr.isEmpty()){
+	            return inputStr;
+	        }      
+		
+		
 		String str="";
         for(int i=inputStr.length()-1;i>=0;i--) {
        	 str=str+inputStr.charAt(i);	 
